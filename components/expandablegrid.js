0184800136c77b51523b668897fd362a8e540c13
vikashiscoder <vikashis@yahoo.com>
2020-10-22 17:00:44 +0530

    1 file modified

--- components/expandablegrid.js
+++ components/expandablegrid.js
@@ -6,19 +6,22 @@ export default class ExpandableGrid extends React.Component {
     this.state = {
       data: [
         {
+          id: 1,
           class: 1,
           name: "One",
-          students: [{ color: "green" }, { color: "yellow" }]
+          nextLevel: [{ color: "green" }, { color: "yellow" }]
         },
         {
+          id: 2,
           class: 2,
           name: "Two",
-          students: [{ color: "green" }, { color: "yellow" }]
+          nextLevel: [{ color: "green" }, { color: "yellow" }]
         },
         {
-          class: 3,
+          id: 3,
+          class:3,
           name: "Three",
-          students: [{ color: "green" }, { color: "yellow" }]
+          nextLevel: [{ color: "green" }, { color: "yellow" }]
         }
       ],
       expandedRows: []
@@ -27,35 +30,44 @@ export default class ExpandableGrid extends React.Component {
 
   handleClick = data => {
     let expandedRowsLocal = [];
-    if (this.state.expandedRows.includes(data.class)) {
-      expandedRowsLocal = this.state.expandedRows.filter(x => x != data.class);
+    if (this.state.expandedRows.includes(data.id)) {
+      expandedRowsLocal = this.state.expandedRows.filter(x => x != data.id);
     } else {
-      expandedRowsLocal = this.state.expandedRows.concat(data.class);
+      expandedRowsLocal = this.state.expandedRows.concat(data.id);
     }
 
     this.setState({ ...this.state, expandedRows: expandedRowsLocal });
   };
 
+  validTDs(data){
+    //id and nextLevel are component specific properties
+    let result = [];
+    for (const prop in data) {
+      if (prop != "id" && prop != "nextLevel") {
+        let element = <td>{data[prop]}</td>;
+        result.push(element);
+      }
+    }
+      return result;
+  };
+
+
   showRow = data => {
     return (
       <React.Fragment>
-        <tr onClick={() => this.handleClick(data)} key={data.class}>
-          <td>{data.class}</td>
-          <td>{data.name}</td>
-          <td />
+        <tr onClick={() => this.handleClick(data)} key={data.id}>
+          {this.validTDs(data)}
         </tr>
-        {this.state.expandedRows.includes(data.class) ? (
-          data.students.map(x=>
-
-          <tr key={data.class + x.color + "expanded"}>
-            <td />
-            <td />
-            <td>{x.color}</td>
-          </tr>
-          )
-          
+        {this.state.expandedRows.includes(data.id) ? (
+          data.nextLevel.map(x => (
+            <tr key={data.id + x.color + "expanded"}>
+              <td />
+              <td />
+              <td>{x.color}</td>
+            </tr>
+          ))
         ) : (
-          ""
+          <React.Fragment />
         )}
       </React.Fragment>
     );
@@ -67,7 +79,7 @@ export default class ExpandableGrid extends React.Component {
         <tr>
           <th>Class</th>
           <th>Name</th>
-          <th style={{ width: "200px"}}>Color</th>
+          <th style={{ width: "200px" }}>Color</th>
         </tr>
         {this.state.data.map(x => this.showRow(x))}
       </table>
